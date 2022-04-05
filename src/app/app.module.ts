f4f031618d8afc56738668c2f985fc4b47fad3f7
justinbartlett9 <coldmilk19@gmail.com>
2022-04-05 15:06:58 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MissionlistComponent, MissiondetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { SpacexapiService } from '../network/spacexapi.service';
+import { Mission } from '../models/mission';
+
+@Component({
+  selector: 'app-missionlist',
+  templateUrl: './missionlist.component.html',
+  styleUrls: ['./missionlist.component.css'],
+})
+export class MissionlistComponent implements OnInit {
+  colours: string[] = [];
+  missions: Mission[] = [];
+
+  constructor(private spacexapiService: SpacexapiService) {}
+
+  ngOnInit(): void {
+    this.getMissions();
+  }
+
+  getMissions(): void {
+    this.spacexapiService.getMissions().subscribe((missions) => {
+      this.missions = missions;
+    });
+  }
+
+  getColours(): void {
+    this.spacexapiService.getColours().subscribe((colours) => {
+      this.colours = colours;
+    });
+  }
+}
